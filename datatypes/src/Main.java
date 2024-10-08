217dd411a0a4d6df73d744565f55d6772d7ba536
AkSysevo <161967650+AkSysevo@users.noreply.github.com>
2024-10-08 16:54:20 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12;
+        //sayi="Ankara"->uyumsuz tipler
+        byte sayi2=15;
+        double sayi3=87.5;
+        //genellikle int ve double kullanılır.
+        String sehir = "Ankara";
+        char karakter = 'A';
+        boolean dogruMu = true;
+        boolean yanlisMi = false;
+
+
+
+    }
+}
